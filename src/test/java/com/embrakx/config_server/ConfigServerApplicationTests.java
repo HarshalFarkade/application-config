8219dd244af79cb7618dc628bfda5aaaac3d7772
Harshal <farkadeharshal14@gmail.com>
2025-05-19 11:38:00 +0530

    done with config-server

---
+++ src/test/java/com/embrakx/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embrakx.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
